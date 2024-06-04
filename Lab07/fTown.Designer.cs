512ff83e44c83fb343f0afd67dc7ca050a8488a5
khlstudy <145128666+nayavua@users.noreply.github.com>
2024-06-04 21:04:24 +0300

    Add project files.

---
+++ Lab07/fTown.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Lab07
+{
+    partial class fTown
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSquare = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtYearIncome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPopulation = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkHasAirport = new System.Windows.Forms.CheckBox();
+            this.chkHasPort = new System.Windows.Forms.CheckBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSquare);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtYearIncome);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtPopulation);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtRegion);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtCountry);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(38, 34);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(504, 308);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Загальні дані";
+            // 
+            // txtSquare
+            // 
+            this.txtSquare.Location = new System.Drawing.Point(210, 211);
+            this.txtSquare.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSquare.Name = "txtSquare";
+            this.txtSquare.Size = new System.Drawing.Size(251, 27);
+            this.txtSquare.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 214);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(128, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Площа, кв. км.";
+            // 
+            // txtYearIncome
+            // 
+            this.txtYearIncome.Location = new System.Drawing.Point(210, 176);
+            this.txtYearIncome.Margin = new System.Windows.Forms.Padding(4);
+            this.txtYearIncome.Name = "txtYearIncome";
+            this.txtYearIncome.Size = new System.Drawing.Size(251, 27);
+            this.txtYearIncome.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 179);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(156, 20);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Річний дохід, грн.";
+            // 
+            // txtPopulation
+            // 
+            this.txtPopulation.Location = new System.Drawing.Point(210, 141);
+            this.txtPopulation.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPopulation.Name = "txtPopulation";
+            this.txtPopulation.Size = new System.Drawing.Size(251, 27);
+            this.txtPopulation.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 144);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Кількість мешканців";
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Location = new System.Drawing.Point(210, 106);
+            this.txtRegion.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(251, 27);
+            this.txtRegion.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 109);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Регіон";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(210, 71);
+            this.txtCountry.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(251, 27);
+            this.txtCountry.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 74);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Країна";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(210, 36);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(251, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Назва міста";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkHasAirport);
+            this.groupBox2.Controls.Add(this.chkHasPort);
+            this.groupBox2.Location = new System.Drawing.Point(38, 350);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(504, 127);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Транспорт";
+            // 
+            // chkHasAirport
+            // 
+            this.chkHasAirport.AutoSize = true;
+            this.chkHasAirport.Location = new System.Drawing.Point(26, 90);
+            this.chkHasAirport.Name = "chkHasAirport";
+            this.chkHasAirport.Size = new System.Drawing.Size(199, 24);
+            this.chkHasAirport.TabIndex = 1;
+            this.chkHasAirport.Text = "Місто має аеропорт";
+            this.chkHasAirport.UseVisualStyleBackColor = true;
+            // 
+            // chkHasPort
+            // 
+            this.chkHasPort.AutoSize = true;
+            this.chkHasPort.Location = new System.Drawing.Point(26, 50);
+            this.chkHasPort.Name = "chkHasPort";
+            this.chkHasPort.Size = new System.Drawing.Size(159, 24);
+            this.chkHasPort.TabIndex = 0;
+            this.chkHasPort.Text = "Місто має порт";
+            this.chkHasPort.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(577, 70);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(142, 27);
+            this.btnOk.TabIndex = 12;
+            this.btnOk.Text = "Ок";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(577, 105);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(142, 27);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "Скасувати";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // fTown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(744, 512);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fTown";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Місто";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSquare;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtYearIncome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPopulation;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRegion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.CheckBox chkHasAirport;
+        private System.Windows.Forms.CheckBox chkHasPort;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
---
+++ Lab07/fTown.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab07
+{
+    public partial class fTown : Form
+    {
+        private Town _town;
+        public fTown(ref Town town)
+        {
+            InitializeComponent();
+            _town = town;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+            string.IsNullOrWhiteSpace(txtCountry.Text) ||
+            string.IsNullOrWhiteSpace(txtRegion.Text) ||
+            !int.TryParse(txtPopulation.Text, out int population) ||
+            !double.TryParse(txtYearIncome.Text, out double yearIncome) ||
+            !double.TryParse(txtSquare.Text, out double square))
+            {
+                MessageBox.Show("Заповніть всі поля у правильному форматі.", "Помилка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            _town.Name = txtName.Text;
+            _town.Country = txtCountry.Text;
+            _town.Region = txtRegion.Text;
+            _town.Population = population;
+            _town.YearIncome = yearIncome;
+            _town.Square = square;
+            _town.HasPort = chkHasPort.Checked;
+            _town.HasAirport = chkHasAirport.Checked;
+
+            this.DialogResult = DialogResult.OK;
+        }
+    }
+    
+}
+
